8e23fb8aab59b4c009dc11ef9676d7231a3c4a2c
Suhaib Akhter <suhaib.akhter@opexustech.com>
2024-12-25 16:29:26 -0500

    first commit

---
+++ __/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyDjjj5H6E6u5d8w5_fNSUOesGFGLTiR_AM",
+  "authDomain": "akhirah-4fa12.firebaseapp.com",
+  "databaseURL": "https://akhirah-4fa12.firebaseio.com",
+  "messagingSenderId": "198495938794",
+  "projectId": "akhirah-4fa12",
+  "storageBucket": "akhirah-4fa12.appspot.com"
+});
